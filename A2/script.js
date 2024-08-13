5c7b49df7cb33e196791bc3226681f72c7084742
João Pedro Alcântara P. Calzá <112181440+Jotinha29@users.noreply.github.com>
2024-08-13 09:50:10 -0300

    Add files via upload

---
+++ A2/script.js
@@ -0,0 +1,12 @@
+var list = ['victor', 'Thais', 'Sabrina', 'Matheus', 150]
+// push()
+// unshift()
+// pop()
+// shift()
+// join()
+// toString() 
+
+var homens = ['homem1', 'homem2','homem3','homem4','homem5']
+var mulher = ['mulher1', 'mulher2','mulher3','mulher4','mulher5']
+
+// concat()
